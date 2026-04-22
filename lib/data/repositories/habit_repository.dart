e920638d86cba84d61c0898e89e7851468a52144
hayrunisaer07-lgtm <hayrunisaer07@gmail.com>
2026-04-23 01:21:00 +0300

    redesigned repo structure to fix a endless bug on app start

--- lib/data/repositories/habit_repository.dart
+++ lib/data/repositories/habit_repository.dart
@@ -4,35 +4,54 @@ import '../models/habit_model.dart';
 import 'package:isar/isar.dart';
 
 class HabitRepository {
-  late Isar isar;
+  Isar? _isar; // Sadece bir degisken Isar in kendisini _isar icine aliyoruz
+
+  Future<Isar> get isar async {
+    // Basit bit getter, _isar degiskenin veri tabanina olan erisimini kontrol eder
+    if (_isar != null) return _isar!;
+    // Eger null degilse o zaman kesin null degil diye return veriyoruz
+    await init();
+    // Bu sart ise yaramazsa, init metoduna geciyoruz
+    return _isar!;
+    // Init bittikten sonra null olmayan _isar i geri veriyoruz
+  }
 
   Future<void> init() async {
-    final dir = await getApplicationDocumentsDirectory();
+    final dir = await getApplicationDocumentsDirectory(); // Init metodumuz
+    // dir adli degiskene veri tabanin addresini atiyor ileride directory ozelligini kullanmak icin
 
-    if (Isar.instanceNames.isEmpty) {
-      isar = await Isar.open([HabitModelSchema], directory: dir.path);
-    } else {
-      isar = Isar.getInstance()!;
+    try {
+      if (Isar.instanceNames.isNotEmpty) {
+        // Isari instance namesi bos degilse yani varsa bir sey
+        // o zaman instansi degiskene aliyoruz
+        _isar = Isar.getInstance();
+      } else {
+        // bossa dbyi aciyoruz ve Habit shemasini tanimliyoruz, dir.path yazarak da onun yerini belirliyoruz
+
+        _isar = await Isar.open([HabitModelSchema], directory: dir.path);
+      }
+    } catch (e) {
+      throw Exception(e);
     }
   }
 
   Future<void> addHabit(HabitModel newHabit) async {
+    final db = await isar;
+
     try {
-      await isar.writeTxn(() async {
-        await isar.habitModels.put(newHabit);
+      await db.writeTxn(() async {
+        await db.habitModels.put(newHabit);
       });
-      print('YENI HABIT DATA BAZAYA ELAVE OLUNDU (HABIT REPOSITORY)');
     } catch (e) {
-      throw Exception(
-        'YENI HABIT DATA BAZAYA ELAVE OLUNMADI (HABIT REPOSITORY) $e',
-      );
+      throw Exception(e);
     }
   }
 
   Future<void> deleteHabit(int habitId) async {
+    final db = await isar;
     try {
-      await isar.writeTxn(() async {
-        await isar.habitModels.delete(habitId);
+      await db.writeTxn(() async {
+        await db.habitModels.delete(habitId);
       });
       print('$habitId idli HABIT SILINDI');
     } catch (e) {
@@ -41,8 +60,9 @@ class HabitRepository {
   }
 
   Future<List<HabitModel>> getAllHabits() async {
+    final db = await isar;
     try {
-      return await isar.habitModels.where().findAll();
+      return await db.habitModels.where().findAll();
     } catch (e) {
       return [];
     }
