b4a3d1b1d9f539d7729ccddedce251398421cfdb
Brave Cat <agalitogrul14@gmail.com>
2026-05-10 01:34:47 +0300

    Update auth_cubit.dart
    added repository class to cubit
    
    added repository.clearAllLocalDb() to logOut() function

--- lib/logic/cubits/auth_cubit.dart
+++ lib/logic/cubits/auth_cubit.dart
@@ -1,29 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import '../../data/repositories/habit_repository.dart';
 
 class AuthCubit extends Cubit<User?> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+  final HabitRepository repository;
 
-  AuthCubit() : super(FirebaseAuth.instance.currentUser);
+  AuthCubit(this.repository) : super(FirebaseAuth.instance.currentUser);
 
   Future<void> signInWithGoogle() async {
     try {
       // 1. Önce kimlik doğrulaması (Authentication) yapıyoruz
-      final GoogleSignInAccount? googleUser = await _googleSignIn.authenticate();
+      final GoogleSignInAccount? googleUser = await _googleSignIn
+          .authenticate();
       if (googleUser == null) return;
 
       // 2. KRİTİK NOKTA: accessToken almak için "authorizeScopes" çağırmalıyız
       // Bu işlem kullanıcıdan e-posta ve profil izni ister.
-      final authorizedUser = await googleUser.authorizationClient.authorizeScopes([
-        'email',
-        'profile',
-      ]);
+      final authorizedUser = await googleUser.authorizationClient
+          .authorizeScopes(['email', 'profile']);
 
       // 3. Token'ları topluyoruz
       final String? idToken = (await googleUser.authentication).idToken;
-      final String? accessToken = authorizedUser.accessToken; // Artık hata vermez!
+      final String? accessToken =
+          authorizedUser.accessToken; // Artık hata vermez!
 
       // 4. Firebase Credential oluşturuyoruz
       final AuthCredential credential = GoogleAuthProvider.credential(
@@ -32,15 +34,17 @@ class AuthCubit extends Cubit<User?> {
       );
 
       // 5. Firebase'e giriş yapıyoruz
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      final UserCredential userCredential = await _auth.signInWithCredential(
+        credential,
+      );
       emit(userCredential.user);
-
     } catch (e) {
       print("Google Login Hatası: $e");
     }
   }
 
   Future<void> logOut() async {
+    await repository.clearAllLocalDb();
     await _googleSignIn.signOut();
     await _auth.signOut();
     emit(null);
