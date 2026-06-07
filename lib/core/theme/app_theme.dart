a6d7021f2b3ed4c29a269a82536eba0b812a2d4f
Brave Cat <agalitogrul14@gmail.com>
2026-06-07 20:27:55 +0300

    Update app_theme.dart
    Added app theme for light and dark mode

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    colorScheme: const ColorScheme.light(
+      primary: AppColors.primary,
+      surface: AppColors.darkSurface,
+      onSurface: AppColors.textLight,
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    colorScheme: const ColorScheme.dark(
+      primary: AppColors.primary,
+      surface: AppColors.darkSurface,
+      onSurface: AppColors.textLight,
+    ),
+  );
+}
